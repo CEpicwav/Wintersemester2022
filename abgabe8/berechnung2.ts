5c5b0b73d0f5654f8028b6e1c0e0dfb4655ef41a
CEpicwav <leondorner@gmx.de>
2022-11-27 22:36:45 +0100

    abgabe 8 dazu

---
+++ abgabe8/berechnung2.ts
@@ -0,0 +1,55 @@
+interface BIP {
+    2015: number;
+    2021: number;
+}
+
+let EU_Mitglieder: Map<string, BIP> = new Map ([
+    ["italy", { 2015: 1.8, 2021: 2.1 }], 
+    ["germany", { 2015: 3.4, 2021: 4.2 }], 
+    ["greece", { 2015: 0.2, 2021: 0.2 }], 
+    ["spain", { 2015: 1.2, 2021: 1.4 }],
+]);
+
+console.log(EU_Mitglieder.get("italy"));
+
+
+
+// Zahlen in Bi. forEach= geht durch jedes einmal durch & speichert was angeguckt wurde in Land
+EU_Mitglieder.forEach(land => {
+    console.log("\n\n" + "Der BIP von "+ land[0] + " war 2015: " + land.bipList[2015]  + " BILLIONEN US DOLLAR gestiegen.");
+    console.log("Der BIP von "+ land.name + " war 2021: " + land.bipList[2021]  + " BILLIONEN US DOLLAR gestiegen.");
+    console.log("Der BIP von Spanien ist seit 2015 um: " + (land.bipList[2021] - land.bipList[2015]).toFixed(1) + " BILLIONEN US DOLLAR gestiegen.");
+});
+
+init();
+let balken: HTMLDivElement;
+
+function init() {
+    let hoverElements = document.getElementsByClassName("wrapper");
+    for (let i = 0; i < hoverElements.length; i++) {
+        hoverElements[i].addEventListener("click", () => {
+            Laender.forEach(land => {
+                if(land.name == hoverElements[i].firstElementChild.className) {
+                    getInformation(land);
+                }
+            });
+        })
+        hoverElements[i].addEventListener("click", getInformationFromName);
+    }
+}
+
+/*function getInformationFromName(_event: Event) {
+    _event.currentTarget
+    Laender.
+}*/
+
+function getInformation(_land: EU_Mitglied) {
+
+    let bip2015 = document.getElementById("bip2015");
+    let bip2021 = document.getElementById("bip2021");
+    bip2015.innerHTML = _land.bipList[2015].toString();
+    bip2021.innerHTML = _land.bipList[2021].toString();
+
+    let balken = document.getElementById("innerBalken");
+    balken.style.width = (100 + (_land.bipList[2021] - _land.bipList[2015])*100).toString()+ "px";
+}
