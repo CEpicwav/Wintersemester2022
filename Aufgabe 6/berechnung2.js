a8c9010028d1b5cdd2d59d31361539be3cce2d57
CEpicwav <leondorner@gmx.de>
2022-11-27 21:59:58 +0100

    abgabe a6 1.0

--- Aufgabe 6/berechnung2.js
+++ Aufgabe 6/berechnung2.js
@@ -1,8 +1,8 @@
 let Laender = [
-    { name: "Italien", bipList: { 2015: 1.8, 2021: 2.1 } },
-    { name: "Deutschland", bipList: { 2015: 3.4, 2021: 4.2 } },
-    { name: "Griechenland", bipList: { 2015: 0.2, 2021: 0.2 } },
-    { name: "Spanien", bipList: { 2015: 1.2, 2021: 1.4 } },
+    { name: "italy", bipList: { 2015: 1.8, 2021: 2.1 } },
+    { name: "germany", bipList: { 2015: 3.4, 2021: 4.2 } },
+    { name: "greece", bipList: { 2015: 0.2, 2021: 0.2 } },
+    { name: "spain", bipList: { 2015: 1.2, 2021: 1.4 } },
 ];
 // Zahlen in Bi. forEach= geht durch jedes einmal durch & speichert was angeguckt wurde in Land
 Laender.forEach(land => {
@@ -10,4 +10,28 @@ Laender.forEach(land => {
     console.log("Der BIP von " + land.name + " war 2021: " + land.bipList[2021] + " BILLIONEN US DOLLAR gestiegen.");
     console.log("Der BIP von Spanien ist seit 2015 um: " + (land.bipList[2021] - land.bipList[2015]).toFixed(1) + " BILLIONEN US DOLLAR gestiegen.");
 });
+init();
+let balken;
+function init() {
+    let hoverElements = document.getElementsByClassName("wrapper");
+    for (let i = 0; i < hoverElements.length; i++) {
+        hoverElements[i].addEventListener("click", () => {
+            Laender.forEach(land => {
+                if (land.name == hoverElements[i].firstElementChild.className) {
+                    getInformation(land);
+                }
+            });
+        });
+    }
+    balken;
+}
+function getInformation(_land) {
+    console.log(_land);
+    let bip2015 = document.getElementById("bip2015");
+    let bip2021 = document.getElementById("bip2021");
+    bip2015.innerHTML = _land.bipList[2015].toString();
+    bip2021.innerHTML = _land.bipList[2021].toString();
+    let balken = document.getElementById("innerBalken");
+    balken.style.width = (100 + (_land.bipList[2021] - _land.bipList[2015]) * 100).toString() + "px";
+}
 //# sourceMappingURL=berechnung2.js.map
