10faf8693d61c45a634e79fcaec31ca89d25cca0
CEpicwav <leondorner@gmx.de>
2022-11-28 00:50:35 +0100

    a8 vorarbeit skrr

--- abgabe8/berechnung2.ts
+++ abgabe8/berechnung2.ts
@@ -11,39 +11,56 @@ namespace abgabe8 {
         ["spain", { 2015: 1.2, 2021: 1.4 }],
     ]);
 
+    let balken: HTMLDivElement;
+
     // Zahlen in Bi. forEach= geht durch jedes einmal durch & speichert was angeguckt wurde in Land
-    EU_Mitglieder.forEach(land => {
-        console.log("\n\n" + "Der BIP von " + land[0] + " war 2015: " + land[1][2015] + " BILLIONEN US DOLLAR gestiegen.");
-        console.log("Der BIP von " + land[0] + " war 2021: " + land[1][2021] + " BILLIONEN US DOLLAR gestiegen.");
-        console.log("Der BIP von Spanien ist seit 2015 um: " + (land[1][2021] - land[1][2015]).toFixed(1) + " BILLIONEN US DOLLAR gestiegen.");
+    EU_Mitglieder.forEach((bip: BIP, land: string) => {
+        console.log("\n\n" + "Der BIP von " + land + " war 2015: " + bip[2015] + " BILLIONEN US DOLLAR gestiegen.");
+        console.log("Der BIP von " + land + " war 2021: " + bip[2021] + " BILLIONEN US DOLLAR gestiegen.");
+        console.log("Der BIP von Spanien ist seit 2015 um: " + (bip[2021] - bip[2015]).toFixed(1) + " BILLIONEN US DOLLAR gestiegen.");
     });
 
     init();
-    let balken: HTMLDivElement;
 
     function init() {
         let hoverElements = document.getElementsByClassName("wrapper");
         for (let i = 0; i < hoverElements.length; i++) {
-            hoverElements[i].addEventListener("click", () => {
-                EU_Mitglieder.get(hoverElements[i].firstElementChild.className);
-            })
             hoverElements[i].addEventListener("click", getInformationFromName);
         }
+        let firstElement: HTMLDivElement = <HTMLDivElement>hoverElements[0];
+        firstElement.click();
     }
 
     function getInformationFromName(_event) {
-        console.log(_event.currentTarget.firstElementChild);
-        EU_Mitglieder.get(_event.currentTarget.firstElementChild);
+        for(let i = 0; i < document.getElementsByClassName("active").length; i++) {
+            document.getElementsByClassName("active")[i].classList.remove("active");
+        }
+            
+        _event.currentTarget.classList.add("active");
+        let childOfClickedElement: HTMLElement = _event.currentTarget.firstElementChild;
+        EU_Mitglieder.forEach((bip: BIP, land: string) => {
+            if (land == childOfClickedElement.className) {
+                getInformation(land, bip);
+            }
+        })
     }
 
-    function getInformation(_land: EU_Mitglied) {
+    function getInformation(_land: string, _bipList: BIP) {
 
         let bip2015 = document.getElementById("bip2015");
         let bip2021 = document.getElementById("bip2021");
-        bip2015.innerHTML = _land.bipList[2015].toString();
-        bip2021.innerHTML = _land.bipList[2021].toString();
+
+        bip2015.innerHTML = _bipList[2015].toString() + " Billionen";
+        bip2021.innerHTML = _bipList[2021].toString() + " Billionen";
 
         let balken = document.getElementById("innerBalken");
-        balken.style.width = (100 + (_land.bipList[2021] - _land.bipList[2015]) * 100).toString() + "px";
+        balken.style.width = (100 + (_bipList[2021] - _bipList[2015]) * 125).toString() + "px";
+        if (_bipList[2021] - _bipList[2015] < 0) {
+            balken.style.backgroundColor = "rgb(200, 0, 0)"
+        } else if (_bipList[2021] - _bipList[2015] > 0) {
+            balken.style.backgroundColor = "rgb(0, 200, 0)"
+        } else {
+            balken.style.backgroundColor = "rgb(200, 200, 0)"
+        }
     }
 }
