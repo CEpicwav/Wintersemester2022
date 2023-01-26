ccafddf12a3ab9f86144a7e0aec65d1632da1b18
CEpicwav <leondorner@gmx.de>
2023-01-27 00:35:41 +0100

    endabgabe_fertig

--- Endabgabe_EIA/questions.ts
+++ Endabgabe_EIA/questions.ts
@@ -7,9 +7,9 @@ class Question {
 
 const htmlQuestions: Question[] = [
     new Question("html", "Wie kann man einen Link in HTML erstellen?", "&lt;a&gt;", "<p>Das hier ist eine Erklärung mit einem Link: <a href='https://de.w3docs.com/html-lernen/html-links.html' target='_blank'>Lösung</a></p>", [`<a>`, `<link>`, `<img>`]),
-    new Question("html", "Wie kann man ein Bild in HTML einfügen?","&lt;img&gt;", "<p>Das hier ist eine Erklärung mit einem Link: <a href='https://www.computerbild.de/artikel/cb-Tipps-Internet-html-bilder-einfuegen-30918285.html' target='_blank'>Lösung</a></p>", [`<a>`, `<img>`, `<navi>`]),
-    new Question("html", "Wie kann man eine Liste in HTML erstellen?","&lt;ul&gt;", "<p>Das hier ist eine Erklärung mit einem Link: <a href='https://praxistipps.chip.de/html-aufzaehlungen-und-listen-einfuegen_46540#:~:text=Aufzählungspunkte%20in%20HTML%20einfügen,Tag%20(s.html' target='_blank'>Lösung</a></p>", [`<ul>`, `<form>`, `<a>`]),
-    new Question("html", "Wie kann man eine Tabelle in HTML erstellen?","&lt;table&gt;", "<p>Das hier ist eine Erklärung mit einem Link: <a href='https://wiki.selfhtml.org/wiki/HTML/Tabellen/Aufbau_einer_Tabelle#:~:text=leitet%20eine%20Tabelle%20ein,Tag%20notiert.html' target='_blank'>Lösung</a></p>", [`<a>`, `<link>`, `<table>`]),
+    new Question("html", "Wie kann man ein Bild in HTML einfügen?", "&lt;img&gt;", "<p>Das hier ist eine Erklärung mit einem Link: <a href='https://www.computerbild.de/artikel/cb-Tipps-Internet-html-bilder-einfuegen-30918285.html' target='_blank'>Lösung</a></p>", [`<a>`, `<img>`, `<navi>`]),
+    new Question("html", "Wie kann man eine Liste in HTML erstellen?", "&lt;ul&gt;", "<p>Das hier ist eine Erklärung mit einem Link: <a href='https://praxistipps.chip.de/html-aufzaehlungen-und-listen-einfuegen_46540#:~:text=Aufzählungspunkte%20in%20HTML%20einfügen,Tag%20(s.html' target='_blank'>Lösung</a></p>", [`<ul>`, `<form>`, `<a>`]),
+    new Question("html", "Wie kann man eine Tabelle in HTML erstellen?", "&lt;table&gt;", "<p>Das hier ist eine Erklärung mit einem Link: <a href='https://wiki.selfhtml.org/wiki/HTML/Tabellen/Aufbau_einer_Tabelle#:~:text=leitet%20eine%20Tabelle%20ein,Tag%20notiert.html' target='_blank'>Lösung</a></p>", [`<a>`, `<link>`, `<table>`]),
     new Question("html", "Wie kann man ein Formular in HTML erstellen?", "&lt;form&gt;", "<p>Das hier ist eine Erklärung mit einem Link: <a href='https://www.html-seminar.de/formulare.html' target='_blank'>Lösung</a></p>", [`<ul>`, `<form>`, `<navi>`]),
 ];
 
@@ -32,9 +32,9 @@ const typescriptQuestions: Question[] = [
 
 const randomQuestions: Question[] = [
     new Question("random", "Wie kann man einen Link in HTML erstellen?", "&lt;a&gt;", "<p>Das hier ist eine Erklärung mit einem Link: <a href='https://de.w3docs.com/html-lernen/html-links.html' target='_blank'>Lösung</a></p>", [`<a>`, `<link>`, `<img>`]),
-    new Question("random", "Wie kann man ein Bild in HTML einfügen?","&lt;img&gt;", "<p>Das hier ist eine Erklärung mit einem Link: <a href='https://www.computerbild.de/artikel/cb-Tipps-Internet-html-bilder-einfuegen-30918285.html' target='_blank'>Lösung</a></p>", [`<a>`, `<img>`, `<navi>`]),
-    new Question("random", "Wie kann man eine Liste in HTML erstellen?","&lt;ul&gt;", "<p>Das hier ist eine Erklärung mit einem Link: <a href='https://praxistipps.chip.de/html-aufzaehlungen-und-listen-einfuegen_46540#:~:text=Aufzählungspunkte%20in%20HTML%20einfügen,Tag%20(s.html' target='_blank'>Lösung</a></p>", [`<ul>`, `<form>`, `<a>`]),
-    new Question("random", "Wie kann man eine Tabelle in HTML erstellen?","&lt;table&gt;", "<p>Das hier ist eine Erklärung mit einem Link: <a href='https://wiki.selfhtml.org/wiki/HTML/Tabellen/Aufbau_einer_Tabelle#:~:text=leitet%20eine%20Tabelle%20ein,Tag%20notiert.html' target='_blank'>Lösung</a></p>", [`<a>`, `<link>`, `<table>`]),
+    new Question("random", "Wie kann man ein Bild in HTML einfügen?", "&lt;img&gt;", "<p>Das hier ist eine Erklärung mit einem Link: <a href='https://www.computerbild.de/artikel/cb-Tipps-Internet-html-bilder-einfuegen-30918285.html' target='_blank'>Lösung</a></p>", [`<a>`, `<img>`, `<navi>`]),
+    new Question("random", "Wie kann man eine Liste in HTML erstellen?", "&lt;ul&gt;", "<p>Das hier ist eine Erklärung mit einem Link: <a href='https://praxistipps.chip.de/html-aufzaehlungen-und-listen-einfuegen_46540#:~:text=Aufzählungspunkte%20in%20HTML%20einfügen,Tag%20(s.html' target='_blank'>Lösung</a></p>", [`<ul>`, `<form>`, `<a>`]),
+    new Question("random", "Wie kann man eine Tabelle in HTML erstellen?", "&lt;table&gt;", "<p>Das hier ist eine Erklärung mit einem Link: <a href='https://wiki.selfhtml.org/wiki/HTML/Tabellen/Aufbau_einer_Tabelle#:~:text=leitet%20eine%20Tabelle%20ein,Tag%20notiert.html' target='_blank'>Lösung</a></p>", [`<a>`, `<link>`, `<table>`]),
     new Question("random", "Wie kann man ein Formular in HTML erstellen?", "&lt;form&gt;", "<p>Das hier ist eine Erklärung mit einem Link: <a href='https://www.html-seminar.de/formulare.html' target='_blank'>Lösung</a></p>", [`<ul>`, `<form>`, `<navi>`]),
     new Question("random", "Wie kann man einen Hintergrundfarbe eines Elements in CSS festlegen?", "&lt;background-color&gt;", "<p>Das hier ist eine Erklärung mit einem Link: <a href='https://wiki.selfhtml.org/wiki/CSS/Tutorials/Hintergrund/Gestaltung_mit_CSS#:~:text=background-color%5BBearbeiten%5D,Element%20eine%20Hintergrundfarbe%20zugewiesen%20werden.&text=In%20diesem%20Beispiel%20erhält%20der,hingegen%20ist%20keine%20Hintergrundfarbe%20festgelegt.html' target='_blank'>Lösung</a></p>", [`<background-color>`, `<font-family>`, `<text-align>`]),
     new Question("random", "Wie kann man die Schriftart eines Elements in CSS festlegen?", "&lt;font-family&gt;", "<p>Das hier ist eine Erklärung mit einem Link: <a href='https://www.edv-lehrgang.de/css-font-family/#:~:text=Die%20Schriftart%20wird%20in%20CSS,mit%20einem%20Komma%20getrennt%20werden.html' target='_blank'>Lösung</a></p>", [`<background-color>`, `<font-family>`, `<text-align>`]),
@@ -77,14 +77,14 @@ function displayQuestion(question: Question) {
     answerOptions[randomQuestionIndex].value = answerOptions[randomQuestionIndex].innerHTML = question.answer;
     select.appendChild(answerOptions[randomQuestionIndex]);
     for (let i = 0; i < answerOptions.length; i++) {
-        if(i == randomQuestionIndex){
-            
+        if (i == randomQuestionIndex) {
+
         } else {
 
             let randomAnswerIndex = Math.floor(Math.random() * currentQuestionnaire.length);
-            if(randomAnswerIndex == randomQuestionIndex) {
+            if (randomAnswerIndex == randomQuestionIndex) {
                 randomAnswerIndex = randomAnswerIndex + 1;
-                if(randomAnswerIndex >= currentQuestionnaire.length) {
+                if (randomAnswerIndex >= currentQuestionnaire.length) {
                     randomAnswerIndex = 0;
                 }
             }
@@ -108,7 +108,7 @@ function getQuestion(topic: string) {
             break;
         case "html":
             possibleQuestions = htmlQuestions;
-            break;    
+            break;
         default:
             possibleQuestions = htmlQuestions;
             break;
@@ -131,7 +131,7 @@ function checkAnswer(answer: string) {
     if (answer === currentQuestion.answer) {
         score = score + 20;
         alert(`Richtig! Ihr aktueller Score ist ${score}`);
-        if(updateScore() == true) {
+        if (updateScore() == true) {
             return;
         };
 
